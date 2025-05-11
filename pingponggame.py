6b904c2a0203606919a31d38f343c9ca87c27eea
Sn0z <155830852+Sn0z@users.noreply.github.com>
2025-05-11 16:56:24 +0545

    Add files via upload
    added framecounter and made it so that the ball speed increases as you play the game.

--- pingponggame.py
+++ pingponggame.py
@@ -1,3 +1,113 @@
+<<<<<<< HEAD
+import cv2
+import cvzone
+from cvzone.HandTrackingModule import HandDetector
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+cap.set(3, 1280)
+cap.set(4, 720)
+
+# Importing all images
+imgBackground = cv2.imread("Resources/Background.png")
+imgGameOver = cv2.imread("Resources/gameOver.png")
+imgBall = cv2.imread("Resources/Ball.png", cv2.IMREAD_UNCHANGED)
+imgBat1 = cv2.imread("Resources/bat1.png", cv2.IMREAD_UNCHANGED)
+imgBat2 = cv2.imread("Resources/bat2.png", cv2.IMREAD_UNCHANGED)
+
+# Hand Detector
+detector = HandDetector(detectionCon=0.8, maxHands=2)
+
+# Variables
+frameCount = 0
+speedI = 200
+ballPos = [100, 100]
+speedX = 15
+speedY = 15
+gameOver = False
+score = [0, 0]
+
+while True:
+    _, img = cap.read()
+    img = cv2.flip(img, 1)
+    imgRaw = img.copy()
+
+    # Find the hand and its landmarks
+    hands, img = detector.findHands(img, flipType=False)  # with draw
+
+    # Overlaying the background image
+    img = cv2.addWeighted(img, 0.2, imgBackground, 0.8, 0)
+
+    # Check for hands
+    if hands:
+        for hand in hands:
+            x, y, w, h = hand['bbox']
+            h1, w1, _ = imgBat1.shape
+            y1 = y - h1 // 2
+            y1 = np.clip(y1, 20, 415)
+
+            if hand['type'] == "Left":
+                img = cvzone.overlayPNG(img, imgBat1, (59, y1))
+                if 59 < ballPos[0] < 59 + w1 and y1 < ballPos[1] < y1 + h1:
+                    speedX = -speedX
+                    ballPos[0] += 30
+                    score[0] += 1
+
+            if hand['type'] == "Right":
+                img = cvzone.overlayPNG(img, imgBat2, (1195, y1))
+                if 1195 - 50 < ballPos[0] < 1195 and y1 < ballPos[1] < y1 + h1:
+                    speedX = -speedX
+                    ballPos[0] -= 30
+                    score[1] += 1
+    frameCount+=1
+
+    if frameCount % speedI ==0:
+        if speedX > 0:
+            speedX += 10
+        else:
+            speedX -= 10
+        if speedY > 0:
+            speedY += 10
+        else:
+            speedY -= 10
+
+    # Game Over
+    if ballPos[0] < 40 or ballPos[0] > 1200:
+        gameOver = True
+
+    if gameOver:
+        img = imgGameOver
+        cv2.putText(img, str(score[1] + score[0]).zfill(2), (585, 360), cv2.FONT_HERSHEY_COMPLEX,
+                    2.5, (200, 0, 200), 5)
+
+    # If game not over move the ball
+    else:
+
+        # Move the Ball
+        if ballPos[1] >= 500 or ballPos[1] <= 10:
+            speedY = -speedY
+
+        ballPos[0] += speedX
+        ballPos[1] += speedY
+
+        # Draw the ball
+        img = cvzone.overlayPNG(img, imgBall, ballPos)
+
+        cv2.putText(img, str(score[0]), (300, 650), cv2.FONT_HERSHEY_COMPLEX, 3, (255, 255, 255), 5)
+        cv2.putText(img, str(score[1]), (900, 650), cv2.FONT_HERSHEY_COMPLEX, 3, (255, 255, 255), 5)
+
+    img[580:700, 20:233] = cv2.resize(imgRaw, (213, 120))
+
+    cv2.imshow("Image", img)
+    key = cv2.waitKey(1)
+    if key == ord('r'):
+        frameCount = 0
+        ballPos = [100, 100]
+        speedX = 15
+        speedY = 15
+        gameOver = False
+        score = [0, 0]
+=======
 import cv2
 import cvzone
 from cvzone.HandTrackingModule import HandDetector
@@ -92,4 +202,5 @@ while True:
         speedY = 15
         gameOver = False
         score = [0, 0]
+>>>>>>> f444e4e7daf8d717d24dec70c3adf3f5006f1231
         imgGameOver = cv2.imread("Resources/gameOver.png")
